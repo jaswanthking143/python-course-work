62fb489f2e616849fdbb7035c67c1d9bdad15fbd
jaswanthking143 <126534770+jaswanthking143@users.noreply.github.com>
2026-06-19 13:01:54 +0530

    Add files via upload

---
+++ day-19/list comprehensions.py
@@ -0,0 +1,26 @@
+res1 = []
+for i in range(1,11):
+    res1.append(i)
+
+res2 =[i for i in range(1,11)]
+
+print(res1)
+print(res2)
+
+res3 = []
+for i in range(3,31,3):
+    res3.append(i)
+
+res4 = [i for i in range(3,31,3)]
+
+print(res3)
+print(res4)
+
+res5 = []
+for i in range(2,51,2):
+    res5.append(i)
+
+res6 = [i for i in range(2,51,2)]
+
+print(res5)
+print(res6)
